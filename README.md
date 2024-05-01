bfd126fa3702919efd092bdf7291ed4835e97d10
liuyaowen <liuyaowen_smile@126.com>
2024-05-01 13:46:29 +0800

    将各个模块拆分，利于管理

--- README.md
+++ README.md
@@ -1,44 +1,50 @@
-# vCard - Personal portfolio
+# 制作自己的个人主页,很漂亮的嘞
+fork from https://github.com/codewithsadee/vcard-personal-portfolio
+#### 本代码将原仓库修改了部分内容
+- 将index拆分，将几个大的类别分别放置，易于维护；
+- 将地图替换为高德接口，国内可以直接访问
+- 添加docker github action，易于部署
 
-![GitHub repo size](https://img.shields.io/github/repo-size/codewithsadee/vcard-personal-portfolio)
-![GitHub stars](https://img.shields.io/github/stars/codewithsadee/vcard-personal-portfolio?style=social)
-![GitHub forks](https://img.shields.io/github/forks/codewithsadee/vcard-personal-portfolio?style=social)
-[![Twitter Follow](https://img.shields.io/twitter/follow/codewithsadee_?style=social)](https://twitter.com/intent/follow?screen_name=codewithsadee_)
-[![YouTube Video Views](https://img.shields.io/youtube/views/SoxmIlgf2zM?style=social)](https://youtu.be/SoxmIlgf2zM)
+部署示例：https://me.liuyaowen.club
 
-vCard is a fully responsive personal portfolio website, responsive for all devices, built using HTML, CSS, and JavaScript.
+**需要的话直接fork过去，点个star**
+## 使用方法
+- 主要代码文件为`index.html`、`about.html`、`resume.html`...等
+- 将你自己的信息填入对应的文件标签中即可，代码很容易看懂
+- 有问题提个issues，回答的很快
 
-## Demo
+## 添加高德地图接口
 
-![vCard Desktop Demo](./website-demo-image/desktop.png "Desktop Demo")
-![vCard Mobile Demo](./website-demo-image/mobile.png "Mobile Demo")
-
-## Prerequisites
-
-Before you begin, ensure you have met the following requirements:
-
-* [Git](https://git-scm.com/downloads "Download Git") must be installed on your operating system.
-
-## Installing vCard
-
-To install **vCard**, follow these steps:
-
-Linux and macOS:
-
-```bash
-sudo git clone https://github.com/codewithsadee/vcard-personal-portfolio.git
+- 在 https://console.amap.com/dev/index 中申请自己的key,很简单，在`我的应用`里面创建一个app，名字随意，获取到自己的apikey（不要使用我的啊）
+- 在 https://developer.amap.com/tools/picker 获取你的坐标
+- 将`contact.html`中的iframe替换为下面代码
+```html
+<iframe
+src="https://restapi.amap.com/v3/staticmap?location=填入你的坐标&zoom=12&size=850*400&markers=mid,,A:填入你的坐标&key=填入你的key"
+width="400" height="300" loading="lazy">
+</iframe>
 ```
+- 如特殊需求见高德官网：https://lbs.amap.com/api/webservice/guide/api/staticmaps/
 
-Windows:
 
-```bash
-git clone https://github.com/codewithsadee/vcard-personal-portfolio.git
-```
+## 部署方法
+- docker部署
 
-## Contact
+假如你已经有自己的服务器了，并且安装了docker
 
-If you want to contact me you can reach me at [Twitter](https://www.twitter.com/codewithsadee).
+在仓库的Repository secrets中添加以下字段
 
-## License
+> DOCKERHUB_PASSWORD donkerhub密码
+> DOCKERHUB_USERNAME donkerhub用户名
+> SERVER_HOST 服务器地址
+> SERVER_PORT ssh端口，没有修改的话就是22
+> SERVER_USER 服务器用户
+> SERVER_PWD 服务器密码
 
-MIT
+OK了，如果你完成了上述操作，在你push代码后会自动的在服务器部署，此时只要使用`服务器地址:8088`就可以访问，如果想要更改访问端口，在`.github\workflows\main.yml`修改`docker run -dp 8088:80  --restart=always --name myprofile liuyaowen0925/myprofile:latest`的端口
+
+- github page 部署
+
+这个就很简单了，直接子仓库设置中的page设置，有很多教程，我就不罗嗦的
+
+请看 https://blog.csdn.net/SiShen654/article/details/132471133
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,63 @@
 
 
 
+// 找到博客标签
+const blogTags = document.querySelector(".blog");
+// 加载HTML页面，挂载上去
+const XML = new XMLHttpRequest();
+XML.open("GET", "/blog.html", true);
+XML.send();
+XML.onreadystatechange = function () {
+  blogTags.innerHTML = XML.responseText;
+}
+
+// 找到portfolio标签
+const portfolioTags = document.querySelector(".portfolio");
+// 加载HTML页面，挂载上去
+const XML2 = new XMLHttpRequest();
+XML2.open("GET", "/portfolio.html", true);
+XML2.send();
+XML2.onreadystatechange = function () {
+  portfolioTags.innerHTML = XML2.responseText;
+}
+
+// 
+const contactTags = document.querySelector(".contact");
+// 加载HTML页面，挂载上去
+const XML3 = new XMLHttpRequest();
+XML3.open("GET", "/contact.html", true);
+XML3.send();
+XML3.onreadystatechange = function () {
+  contactTags.innerHTML = XML3.responseText;
+}
+
+// 简历
+const resumeTags = document.querySelector(".resume");
+// 加载HTML页面，挂载上去
+const XML4 = new XMLHttpRequest();
+XML4.open("GET", "/resume.html", true);
+XML4.send();
+XML4.onreadystatechange = function () {
+  resumeTags.innerHTML = XML4.responseText;
+}
+
+// 关于
+const aboutTags = document.querySelector(".about");
+// 加载HTML页面，挂载上去
+const XML5 = new XMLHttpRequest();
+XML5.open("GET", "/about.html", true);
+XML5.send();
+XML5.onreadystatechange = function () {
+  aboutTags.innerHTML = XML5.responseText;
+}
+
+
+
+
+
+
+
+
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
@@ -56,113 +113,117 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 
 
 // custom select variables
-const select = document.querySelector("[data-select]");
-const selectItems = document.querySelectorAll("[data-select-item]");
-const selectValue = document.querySelector("[data-selecct-value]");
-const filterBtn = document.querySelectorAll("[data-filter-btn]");
+window.onload = function () {
+  const select = document.querySelector("[data-select]");
+  const selectItems = document.querySelectorAll("[data-select-item]");
+  const selectValue = document.querySelector("[data-selecct-value]");
+  const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-select.addEventListener("click", function () { elementToggleFunc(this); });
+  window.onload = function () {
+    select.addEventListener("click", function () { elementToggleFunc(this); });
+  }
 
-// add event in all select items
-for (let i = 0; i < selectItems.length; i++) {
-  selectItems[i].addEventListener("click", function () {
+  // add event in all select items
+  for (let i = 0; i < selectItems.length; i++) {
+    selectItems[i].addEventListener("click", function () {
 
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    elementToggleFunc(select);
-    filterFunc(selectedValue);
+      let selectedValue = this.innerText.toLowerCase();
+      selectValue.innerText = this.innerText;
+      elementToggleFunc(select);
+      filterFunc(selectedValue);
 
-  });
-}
+    });
+  }
 
-// filter variables
-const filterItems = document.querySelectorAll("[data-filter-item]");
-console.log(filterItems);
+  // filter variables
+  const filterItems = document.querySelectorAll("[data-filter-item]");
+  console.log(filterItems);
 
-const filterFunc = function (selectedValue) {
-  console.log(selectedValue);
+  const filterFunc = function (selectedValue) {
+    console.log(selectedValue);
 
-  for (let i = 0; i < filterItems.length; i++) {
+    for (let i = 0; i < filterItems.length; i++) {
 
-    if (selectedValue === "所有") {
-      filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
-      filterItems[i].classList.add("active");
-    } else {
-      filterItems[i].classList.remove("active");
+      if (selectedValue === "所有") {
+        filterItems[i].classList.add("active");
+      } else if (selectedValue === filterItems[i].dataset.category) {
+        filterItems[i].classList.add("active");
+      } else {
+        filterItems[i].classList.remove("active");
+      }
+
     }
 
   }
 
-}
+  // add event in all filter button items for large screen
+  let lastClickedBtn = filterBtn[0];
 
-// add event in all filter button items for large screen
-let lastClickedBtn = filterBtn[0];
+  for (let i = 0; i < filterBtn.length; i++) {
 
-for (let i = 0; i < filterBtn.length; i++) {
+    filterBtn[i].addEventListener("click", function () {
 
-  filterBtn[i].addEventListener("click", function () {
+      let selectedValue = this.innerText.toLowerCase();
+      selectValue.innerText = this.innerText;
+      filterFunc(selectedValue);
 
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    filterFunc(selectedValue);
+      lastClickedBtn.classList.remove("active");
+      this.classList.add("active");
+      lastClickedBtn = this;
 
-    lastClickedBtn.classList.remove("active");
-    this.classList.add("active");
-    lastClickedBtn = this;
+    });
 
-  });
-
-}
+  }
 
 
 
-// contact form variables
-const form = document.querySelector("[data-form]");
-const formInputs = document.querySelectorAll("[data-form-input]");
-const formBtn = document.querySelector("[data-form-btn]");
+  // contact form variables
+  const form = document.querySelector("[data-form]");
+  const formInputs = document.querySelectorAll("[data-form-input]");
+  const formBtn = document.querySelector("[data-form-btn]");
 
-// add event to all form input field
-for (let i = 0; i < formInputs.length; i++) {
-  formInputs[i].addEventListener("input", function () {
+  // add event to all form input field
+  for (let i = 0; i < formInputs.length; i++) {
+    formInputs[i].addEventListener("input", function () {
 
-    // check form validation
-    if (form.checkValidity()) {
-      formBtn.removeAttribute("disabled");
-    } else {
-      formBtn.setAttribute("disabled", "");
-    }
-
-  });
-}
-
-// add event to form button
-formBtn.addEventListener("click", function () {
-  // 将内容发送到邮箱
-
-  form.reset();
-  formBtn.setAttribute("disabled", "");
-});
-
-
-// page navigation variables
-const navigationLinks = document.querySelectorAll("[data-nav-link]");
-const pages = document.querySelectorAll("[data-page]");
-
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
-
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
+      // check form validation
+      if (form.checkValidity()) {
+        formBtn.removeAttribute("disabled");
       } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
+        formBtn.setAttribute("disabled", "");
       }
-    }
 
+    });
+  }
+
+  // add event to form button
+  formBtn.addEventListener("click", function () {
+    // 将内容发送到邮箱
+
+    form.reset();
+    formBtn.setAttribute("disabled", "");
   });
+
+
+  // page navigation variables
+  const navigationLinks = document.querySelectorAll("[data-nav-link]");
+  const pages = document.querySelectorAll("[data-page]");
+
+  // add event to all nav link
+  for (let i = 0; i < navigationLinks.length; i++) {
+    navigationLinks[i].addEventListener("click", function () {
+
+      for (let i = 0; i < pages.length; i++) {
+        if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+          pages[i].classList.add("active");
+          navigationLinks[i].classList.add("active");
+          window.scrollTo(0, 0);
+        } else {
+          pages[i].classList.remove("active");
+          navigationLinks[i].classList.remove("active");
+        }
+      }
+
+    });
+  }
 }
